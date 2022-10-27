a6491fce3a732974446813598c3271adfb1c615f
camilly2 <camillysugai.9445@aluno.saojudas.br>
2022-10-27 12:08:49 -0300

    final_27_10

---
+++ front/medicos/src/app/services/medico/medico.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Medico } from 'src/app/model/medico/medico';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MedicoService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  listaDeMedicos() {
+    return this.httpClient.get<Medico>('htpp://localhost:3000/medicos')
+      .pipe(
+        (res) => res,
+        (err) => err
+      );
+  }
+}
---
+++ front/medicos/src/app/views/lmedicos/lmedicos.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { catchError, Observable, of } from 'rxjs';
+import { Medico } from 'src/app/model/medico/medico';
+import { MedicoService } from 'src/app/services/medico/medico.service';
+
+@Component({
+  selector: 'app-lmedicos',
+  templateUrl: './lmedicos.component.html',
+  styleUrls: ['./lmedicos.component.css']
+})
+export class LmedicosComponent implements OnInit {
+
+  medicos$!: Observable<Medico[]>;
+  constructor(private serviceMedico: MedicoService) {
+    this.serviceMedico.listaDeMedicos()
+      .pipe(
+        catchError((err) => {
+          console.log(err);
+          return of([]);
+        }
+        )
+      )
+  }
+
+  ngOnInit(): void {
+  }
+
+  lista(){
+    const jose =this.medicos$.subscribe(
+      (res) => console.log(res),
+    )
+    console.log(jose);
+  }
+
+}
